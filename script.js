0effa5015fa984e242570ca000bb778a89316224
clarence101701 <clarence.coronell@gmail.com>
2023-06-18 10:36:23 +0800

    Add input filter

--- script.js
+++ script.js
@@ -22,41 +22,121 @@ const dlt = document.querySelector("#delete");
 const equation = document.querySelector("#equation");
 const result = document.querySelector("#result");
 const numbers = document.querySelectorAll('.key--num');
+const zero = document.querySelector('#zero');
 
 let zeroIsOn = true;
 let tempNumber = "";
 let display = "";
 let equationContent = [];
+let pointIsAvail = true;
 
 
 numbers.forEach((num) =>{
     num.addEventListener('click', () => {
-        update(num.innerHTML);
+        if(display[display.length-1] != '%'){
+            update(num.innerHTML);
+        }
+        else{
+            num.style.backgroundColor = 'rgb(224, 72, 61)';
+            setTimeout(()=>{
+                num.style.backgroundColor = 'rgb(80, 81, 82)';
+            }, 300)
+        }
     });
 });
 
+zero.addEventListener('click', () => {
+    if(display.length != 0 && display[display.length-1] != ' '){
+        update(zero.innerHTML);
+    }
+    else{
+        zero.style.backgroundColor = 'rgb(224, 72, 61)';
+        setTimeout(()=>{
+            zero.style.backgroundColor = 'rgb(80, 81, 82)';
+        }, 300)
+    }
+});
+
 plus.addEventListener('click', ()=>{
-    update(' + ');
+    if(display[display.length-1] != ' ' && display[display.length-1] != '.' && display.length != 0){
+        update(' + ');
+        pointIsAvail = true;
+    }
+    else{
+        plus.style.backgroundColor = 'rgb(224, 72, 61)';
+        setTimeout(()=>{
+            plus.style.backgroundColor = 'rgb(80, 81, 82)';
+        }, 300)
+    }
 });
 
 minus.addEventListener('click', ()=>{
-    update(' - ');
+    if(display[display.length-1] != ' ' && display[display.length-1] != '.' && display.length != 0){
+        update(' - ');
+        pointIsAvail = true;
+    }
+    else{
+        minus.style.backgroundColor = 'rgb(224, 72, 61)';
+        setTimeout(()=>{
+            minus.style.backgroundColor = 'rgb(80, 81, 82)';
+        }, 300)
+    }
 });
 
 multiply.addEventListener('click', ()=>{
-    update(' * ');
+    if(display[display.length-1] != ' ' && display[display.length-1] != '.' && display.length != 0){
+        update(' * ');
+        pointIsAvail = true;
+    }
+    else{
+        multiply.style.backgroundColor = 'rgb(224, 72, 61)';
+        setTimeout(()=>{
+            multiply.style.backgroundColor = 'rgb(80, 81, 82)';
+        }, 300)
+    }
 });
 
 divide.addEventListener('click', ()=>{
-    update(' / ');
+    if(display[display.length-1] != ' ' && display[display.length-1] != '.' && display.length != 0){
+        update(' / ');
+        pointIsAvail = true;
+    }
+    else{
+        divide.style.backgroundColor = 'rgb(224, 72, 61)';
+        setTimeout(()=>{
+            divide.style.backgroundColor = 'rgb(80, 81, 82)';
+        }, 300)
+    }
 });
 
 point.addEventListener('click', ()=> {
-    update('.');
+
+    if(!pointIsAvail) {
+        point.style.backgroundColor = 'rgb(224, 72, 61)';
+        setTimeout(()=>{
+            point.style.backgroundColor = 'rgb(80, 81, 82)';
+        }, 300)
+        return;
+    };
+    if(display[display.length-1] == ' ' || display.length == 0){
+        update('0');
+    }
+
+    update(point.innerHTML);
+    pointIsAvail = false;
+    
 });
 
 percent.addEventListener('click', ()=> {
-    update('%');
+    if(display[display.length-1] != ' ' && display[display.length-1] != '%' && display.length != 0 && display[display.length-1] != '.'){
+        update('%');
+    }
+    else{
+        percent.style.backgroundColor = 'rgb(224, 72, 61)';
+        setTimeout(()=>{
+            percent.style.backgroundColor = 'rgb(80, 81, 82)';
+        }, 300)
+    }
 });
 
 clear.addEventListener('click', ()=> {
@@ -64,9 +144,15 @@ clear.addEventListener('click', ()=> {
      equation.innerHTML = "";
      result.innerHTML = "";
      display = "";
+     pointIsAvail = true;
 })
 
 dlt.addEventListener('click', ()=> {
+
+    if(display[display.length-1] == '.' && display[display.length-2] == '0'){
+        display = display.slice(0, display.length-2);
+    }
+
     if(display[display.length-1] == ' '){
         display = display.slice(0, display.length-3);
     }
